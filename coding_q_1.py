3d9be17c4790c1267bb98a9d1f0fc35960035c7a
Mariusz Cichocki-Kaiser <kaiser.mariusz@gmail.com>
2018-01-09 14:48:35 +0100

    Update coding_q_1.py

--- coding_q_1.py
+++ coding_q_1.py
@@ -13,6 +13,6 @@ Consider use range(#begin, #end) method
 result = []                                         #creating empty list to store result
 for i in range(2000, 3201):                         #iterating in given range from 2000 to 3200 (2000 < 3201)
     if i%7 == 0 and i%5 != 0:                       #if remainder of 7 and i equals to zero AND remainder of 5 and i not equals to 0
-        result.append(str(i))                       #then append result of if statement to a list
+        result.append(str(i))                       #then append result of 'if' statement to a list
 
 print(', '.join(result))                            #printing result in a comma-separated sequence
